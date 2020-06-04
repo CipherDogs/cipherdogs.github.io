6bc741db0b30e3822d520f37c5a70b92e5bd81af
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-06-04 11:10:52 +0700

    Change Links

--- src/Links.elm
+++ src/Links.elm
@@ -7,7 +7,7 @@ import Html.Attributes exposing (class, href)
 view : Html msg
 view =
     Html.div [ class "block" ]
-        [ Html.p [] [ text "Our team is interested in electronic art/hack/social activity and blockchain/decentralized technology and other technologies. We are aimed at promoting blockchain technology among people. Our team creates various libraries, websites, artwork and other projects in the field of blockchain technologies. We also help blockchain projects. We believe that blockchain technology is the future." ]
+        [ Html.h3 [] [ text "Projects and Art" ]
         , Html.p []
             [ Html.a [ href "https://github.com/CipherDogs/manifest" ] [ text "Manifest" ]
             , text " | "
